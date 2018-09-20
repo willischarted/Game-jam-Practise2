5801a9ada54330d6c5c04e1d22a2f164a7de9577
William <willischarted@gmail.com>
2018-09-20 13:35:40 -0400

    Physics material added
    script creation and physics materials

---
+++ Extremely Competetive 4-Square/Assets/MouseController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseController : MonoBehaviour {
+
+	public GameObject particle;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetButtonDown("Fire1"))
+        {
+			RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			
+			if (Physics.Raycast(ray, out hit)) {
+                if (hit.rigidbody != null)
+          			Instantiate(particle, hit.point, Quaternion.identity);
+				if (hit.rigidbody.gameObject.name == "Ball")
+					Debug.Log("Hit the ball");
+			}
+        }
+	}
+}
